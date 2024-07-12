362aa98d0f55c8ee89dd0bb0bdcf92ba4d72d7bc
rayfiyo <rayfiyorex@gmail.com>
2024-07-12 18:13:14 +0900

    feat: 🍻 swagger の追加

--- cmd/main.go
+++ cmd/main.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	_ "github.com/rayfiyo/kotatuneko-rest/docs"
 	"github.com/rayfiyo/kotatuneko-rest/internal/app/handler/hello"
+	echoswagger "github.com/swaggo/echo-swagger"
 )
+
+// @title			Kotatuneko REST API
+// @version			1.0
+// @description		こたつねこの REST API
+// @termsOfService	http://localhost:8080/
+
+// @license.name	BSD-3-Clause license
+// @license.url		https://github.com/rayfiyo/kotatuneko-rest?tab=BSD-3-Clause-1-ov-file
+
+// @host						localhost:8080
+// @BasePath					/
+// @SecurityDefinitions.apiKey	Bearer
+// @in							header
+// @name						Authorization
 
 func main() {
 	e := echo.New()
 
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	e.GET("/", hello.Hello)
+	e.GET("/hello", hello.Hello)
+	e.GET("/swagger/*", echoswagger.WrapHandler)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
--- internal/app/handler/hello/handler.go
+++ internal/app/handler/hello/handler.go
@@ -4,7 +4,20 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	_ "github.com/rayfiyo/kotatuneko-rest/docs"
 )
+
+// @Summary		Hello
+// @Description	Hello World 用
+// @Tags		Hello
+// @Accept		string
+// @Produce		string
+
+// @Success	200	{string}	string	"OK"
+// @Failure	400	{string}	string	"Bad Request"
+// @Failure	404	{string}	string	"Not Found"
+
+// @Router		/hello [get]
 
 func Hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
