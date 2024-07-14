accf8271fb6d26db54a36ec5372e82b22afb5e0b
rayfiyo <rayfiyorex@gmail.com>
2024-07-14 16:14:16 +0900

    feat: 🍻 Create と SelectByID のリポジトリインターフェースの定義

--- internal/domain/repository/user.go
+++ internal/domain/repository/user.go
@@ -3,12 +3,12 @@ package user
 import (
 	"context"
 
-	"github.com/rayfiyo/kotatuneko-rest/proto"
+	entity "github.com/rayfiyo/kotatuneko-rest/gen/user/resources"
 )
 
 type IRepository interface {
-	Create(ctx context.Context, user *proto.User) error
-	GetByID(ctx context.Context, id string) (*proto.User, error)
-	Update(ctx context.Context, user *proto.User) error
-	Delete(ctx context.Context, id *string) error
+	Create(ctx context.Context, userName string) (*entity.User, error)
+	SelectByID(ctx context.Context, userId string) (*entity.User, error)
+	// Update(ctx context.Context, user *entity.User) error
+	// Delete(ctx context.Context, userId *string) error
 }
