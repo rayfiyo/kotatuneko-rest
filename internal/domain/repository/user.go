5c9c2e8807047ef84b4930ed4ec117f79f47d22c
rayfiyo <rayfiyorex@gmail.com>
2024-07-15 00:27:41 +0900

    refactor: 💡 型の調整とか？

--- internal/domain/repository/user.go
+++ internal/domain/repository/user.go
@@ -1,14 +1,12 @@
-package user
+package repository
 
 import (
-	"context"
-
 	entity "github.com/rayfiyo/kotatuneko-rest/gen/user/resources"
 )
 
-type IRepository interface {
-	Create(ctx context.Context, userName string) (*entity.User, error)
-	SelectByID(ctx context.Context, userId string) (*entity.User, error)
-	// Update(ctx context.Context, user *entity.User) error
-	// Delete(ctx context.Context, userId *string) error
+type UserRepository interface {
+	CreateUser(userName string) (*entity.User, error)
+	SelectUserByID(userId string) (*entity.User, error)
+	UpdateUser(user *entity.User) error
+	DeleteUser(userId string) error
 }
