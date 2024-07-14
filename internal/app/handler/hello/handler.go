fb62c2a5a11601bc0507c7f84e7bae557e85e30f
rayfiyo <rayfiyorex@gmail.com>
2024-07-15 00:26:55 +0900

    refactor: 💡 entity ディレクトリ削除に伴って entity の追加

--- internal/app/handler/hello/handler.go
+++ internal/app/handler/hello/handler.go
@@ -5,8 +5,20 @@ import (
 
 	"github.com/labstack/echo/v4"
 	_ "github.com/rayfiyo/kotatuneko-rest/docs"
-	"github.com/rayfiyo/kotatuneko-rest/internal/domain/entity"
+	// "github.com/rayfiyo/kotatuneko-rest/internal/domain/entity"
 )
+
+type Response struct {
+	// "github.com/rayfiyo/kotatuneko-rest/internal/domain/entity"
+	Int64  int64  `json:"int64"`
+	String string `json:"string"`
+	World  *Item  `json:"world"`
+}
+
+type Item struct {
+	// "github.com/rayfiyo/kotatuneko-rest/internal/domain/entity"
+	Text string `json:"text"`
+}
 
 // @Summary		Hello
 // @Description	Hello World 用
@@ -20,10 +32,10 @@ import (
 
 // @Router	/hello	[get]
 func Hello(c echo.Context) error {
-	return c.JSON(http.StatusOK, &entity.Response{
+	return c.JSON(http.StatusOK, &Response{
 		Int64:  1,
 		String: "example",
-		World: &entity.Item{
+		World: &Item{
 			Text: "hello world !",
 		},
 	})
