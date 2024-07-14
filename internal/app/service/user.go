447b85dcd2523dc40ca58ae3fe3df1a0d4e21b77
rayfiyo <rayfiyorex@gmail.com>
2024-07-15 00:22:48 +0900

    feat: 🍻 service の作成

--- internal/app/service/user.go
+++ internal/app/service/user.go
@@ -1,17 +1,49 @@
 package service
 
 import (
-	"context"
-
+	entity "github.com/rayfiyo/kotatuneko-rest/gen/user/resources"
+	schema "github.com/rayfiyo/kotatuneko-rest/gen/user/rpc"
 	"github.com/rayfiyo/kotatuneko-rest/internal/domain/repository"
-	"github.com/rayfiyo/kotatuneko-rest/proto"
 )
 
-type IUserService interface {
-	CreateByID(ctx context.Context, userID string) error
-	GetByID(ctx context.Context, userID string) (*proto.User, error)
-	// Update(ctx context.Context, userID string, input *schema.UpdateUserInput) error
+type UserService struct {
+	userRepository repository.UserRepository
 }
 
-func New(repo user.IRepository) {
+func NewUserService(userRepo repository.UserRepository) *UserService {
+	return &UserService{userRepository: userRepo}
+}
+
+func (s *UserService) CreateUser(userName string) (*entity.User, error) {
+	user, err := s.userRepository.CreateUser(userName)
+	if err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+func (s *UserService) GetUserByID(userId string) (*entity.User, error) {
+	user, err := s.userRepository.SelectUserByID(userId)
+	if err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+func (s *UserService) UpdateUser(req *schema.UpdateUserRequest) error {
+	user := &entity.User{
+		Id:   req.User.Id,
+		Name: req.User.Name,
+	}
+	if err := s.userRepository.UpdateUser(user); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *UserService) DeleteUser(userID string) error {
+	if err := s.userRepository.DeleteUser(userID); err != nil {
+		return err
+	}
+	return nil
 }
