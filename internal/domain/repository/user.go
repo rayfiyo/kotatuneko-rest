aa3426c38c5df9ed9b9f7e0b4dbab47a4903382a
rayfiyo <rayfiyorex@gmail.com>
2024-07-15 19:14:34 +0900

    feat: 🍻 Domain service の実装

--- internal/domain/repository/user.go
+++ internal/domain/repository/user.go
@@ -1,12 +1,14 @@
 package repository
 
 import (
+	"github.com/google/uuid"
 	entity "github.com/rayfiyo/kotatuneko-rest/gen/user/resources"
 )
 
 type UserRepository interface {
-	CreateUser(user *entity.User) error
-	SelectUserByID(userId string) (*entity.User, error)
-	UpdateUser(user *entity.User) error
-	DeleteUser(userId string) error
+	Create(user *entity.User) (*entity.User, error)
+	SelectByID(userId uuid.UUID) (*entity.User, error)
+	SelectByName(userName string) (*entity.User, error)
+	// Update(user *entity.User) (*entity.User, error)
+	Delete(user *entity.User) error
 }
---
+++ internal/domain/service/user.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+
+	// "github.com/google/uuid"
+	entity "github.com/rayfiyo/kotatuneko-rest/gen/user/resources"
+	// schema "github.com/rayfiyo/kotatuneko-rest/gen/user/rpc"
+	// "github.com/rayfiyo/kotatuneko-rest/internal/domain/repository"
+)
+
+type User struct {
+	entityUser  *entity.User
+	userService *UserService
+}
+
+func NewUser(user *entity.User) *User {
+	return &User{entityUser: user}
+}
+
+// 認証
+func (u *User) Authenticate() (bool, error) {
+	return u.userService.VerifyUserCredentials(u.entityUser.Id, u.entityUser.Password)
+}
+
+// ハッシュ化
+func (u *User) Hashing() error {
+	hashed, err := u.userService.HashPassword(u.entityUser.Password)
+	if err != nil {
+		return err
+	}
+	u.entityUser.Password = hashed
+	return nil
+}
