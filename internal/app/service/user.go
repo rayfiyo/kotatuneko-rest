37f828b22d440426c80eeba02def1c95e3dd0f51
rayfiyo <rayfiyorex@gmail.com>
2024-07-15 11:45:35 +0900

    refactor: 💡 uuid生成の追加など

--- internal/app/service/user.go
+++ internal/app/service/user.go
@@ -1,6 +1,9 @@
 package service
 
 import (
+	"log"
+
+	"github.com/google/uuid"
 	entity "github.com/rayfiyo/kotatuneko-rest/gen/user/resources"
 	schema "github.com/rayfiyo/kotatuneko-rest/gen/user/rpc"
 	"github.com/rayfiyo/kotatuneko-rest/internal/domain/repository"
@@ -15,17 +18,25 @@ func NewUserService(userRepo repository.UserRepository) *UserService {
 }
 
 func (s *UserService) CreateUser(userName string) (*entity.User, error) {
-	user, err := s.userRepository.CreateUser(userName)
-	if err != nil {
-		return user, err
+	// UUID の生成
+	userID := uuid.New().String()
+	user := &entity.User{
+		Id:   userID,
+		Name: userName,
+	}
+
+	if err := s.userRepository.CreateUser(user); err != nil {
+		log.Printf("Error creating user: %v", err)
+		return nil, err
 	}
 	return user, nil
 }
 
-func (s *UserService) GetUserByID(userId string) (*entity.User, error) {
-	user, err := s.userRepository.SelectUserByID(userId)
+func (s *UserService) GetUserByID(userID string) (*entity.User, error) {
+	user, err := s.userRepository.SelectUserByID(userID)
 	if err != nil {
-		return user, err
+		log.Printf("Error getting user by ID: %v", err)
+		return nil, err
 	}
 	return user, nil
 }
@@ -36,6 +47,7 @@ func (s *UserService) UpdateUser(req *schema.UpdateUserRequest) error {
 		Name: req.User.Name,
 	}
 	if err := s.userRepository.UpdateUser(user); err != nil {
+		log.Printf("Error updating user: %v", err)
 		return err
 	}
 	return nil
@@ -43,6 +55,7 @@ func (s *UserService) UpdateUser(req *schema.UpdateUserRequest) error {
 
 func (s *UserService) DeleteUser(userID string) error {
 	if err := s.userRepository.DeleteUser(userID); err != nil {
+		log.Printf("Error deleting user: %v", err)
 		return err
 	}
 	return nil
